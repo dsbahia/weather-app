8d0806ba5d7366a915533e1dc9b84f7e23b8739e
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-07 21:19:53 +0100

    Created ForecastSummary component with BEM naming convention

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -6,10 +6,12 @@ function ForecastSummary(props) {
     <div className="forecast-summary">
       <div className="forecast-summary__date">{date}</div>
       <div className="forecast-summary__icon">{icon}</div>
-      <div className="forecast-summary__temperature">{temperature.max}&deg;C</div>
+      <div className="forecast-summary__temperature">
+        {temperature.max}&deg;C
+      </div>
       <div className="forecast-summary__description">{description}</div>
     </div>
   );
-
+}
 
 export default ForecastSummary;
