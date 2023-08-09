9a01bf1825cf09961f92fcc9012b75d5575d1536
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-09 21:02:47 +0100

    Snapchat matchers created in test file

--- src/tests/ForecastSummary.test.js
+++ src/tests/ForecastSummary.test.js
@@ -3,7 +3,24 @@ import { render } from "@testing-library/react";
 import ForecastSummary from "../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
+  const validProps = {
+    date: 1111111,
+    description: "Test Description",
+    icon: "testIcon",
+    temperature: {
+      min: 10,
+      max: 20,
+    },
+  };
   it("renders forecast summary", () => {
-    render(<ForecastSummary />);
+    const { rendered } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />,
+    );
+    expect(rendered).toMatchSnapshot();
   });
 });
