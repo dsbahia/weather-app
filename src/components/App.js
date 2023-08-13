bf1f9e46bf0a1b845c03a50813166bc8317ec3c4
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-13 11:03:17 +0100

    Imported useState() into App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import "../styles/App.css";
-import React from "react";
+import React, { useState } from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
 function App({ forecasts, location }) {
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate,
+  );
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
-      <ForecastSummaries forecasts={forecasts} />
-      {forecasts.length > 0 && <ForecastDetails forecast={forecasts[0]} />}
+      <ForecastSummaries
+        forecasts={forecasts}
+        setSelectedDate={setSelectedDate}
+      />
+      {selectedForecast && <ForecastDetails forecast={selectedForecast} />}
+      <button type="button" onClick={() => console.log("Testing")}>
+        More details
+      </button>
     </div>
   );
 }
