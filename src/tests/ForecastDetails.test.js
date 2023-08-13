bbb05e8104f70776fa5b286ace202eef68e19f01
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-13 11:14:05 +0100

    Test written for Forecast Details

--- src/tests/ForecastDetails.test.js
+++ src/tests/ForecastDetails.test.js
@@ -3,5 +3,19 @@ import { render } from "@testing-library/react";
 import ForecastDetails from "../components/ForecastDetails";
 
 describe("ForecastDetails", () => {
-  it("xxxx", () => {});
+  it("renders forecast details correctly", () => {
+    const forecast = {
+      date: "2023-08-13",
+      temperature: { max: 28, min: 18 },
+      humidity: 60,
+      wind: { speed: 15, direction: "NE" },
+    };
+    const { getByText } = render(<ForecastDetails forecast={forecast} />);
+
+    expect(getByText("Max Temperature: 28°C")).toBeInTheDocument();
+    expect(getByText("Min Temperature: 18°C")).toBeInTheDocument();
+    expect(getByText("Humidity: 60%")).toBeInTheDocument();
+    expect(getByText("Wind Speed: 15 km/h")).toBeInTheDocument();
+    expect(getByText("Wind Direction: NE")).toBeInTheDocument();
+  });
 });
